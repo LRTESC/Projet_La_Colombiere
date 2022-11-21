bf5fb00fef1395c8d918af38e283678068b824cc
bricec-campus <brice.chaves@le-campus-numerique.fr>
2022-11-21 11:50:17 +0100

    Step 3 : Proper Views

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function show()
+    {
+            return view("product-list");
+    }
+    public function id($id)
+    {
+            return view("product-details",[
+                'id' => $id
+            ]);
+    }
+}
