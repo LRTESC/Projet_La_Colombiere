54eaa0d56682dd033cbe682855b9a45b13229f71
bricec-campus <brice.chaves@le-campus-numerique.fr>
2022-11-22 17:00:15 +0100

    Correction header footer CSS

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -14,7 +14,9 @@
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
     <!-- CSS -->
+    <link rel="stylesheet" href="{{asset('css/header.css')}}">
     <link rel="stylesheet" href="{{asset('css/'.$css.'.css')}}">
+    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
