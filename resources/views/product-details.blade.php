dc078431e5b12ec7beca3c71650a40c4aa5c359a
DenyApoptose <souheil.sirbal@le-campus-numerique.fr>
2022-11-22 16:53:13 +0100

    adding html - css / relocate img 22/11/2022

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -14,16 +14,29 @@
 <body>
 <h1>{{$id}}</h1>
 
+
+
 <div class="parent">
     <div class="item1">
-
+        <div> IMAGE 1</div>
+        <div> IMAGE 2</div>
+        <div> IMAGE 3</div>
+        <div> IMAGE 4</div>
     </div>
     <div class="item2">
-      <img src="../pictures/STEREO_11.jpg" alt="">
+      <img src="../storage/pictures/STEREO_11.jpg" alt="">
 
     </div>
     <div class="item3">
-        <div> Titre du produit <br> choix du modèle <br> bouton modèle <br> bouton ajouter au panier <br> infos livraion et paiment</div>
+        <section class='FlexContainer'>
+
+            <div>Phantom I 103 dB - Stereo </div>
+            <div>vivez une expérience immersive hors du commun.</div>
+            <div class="mono-stereo"><button>MONO</button><button>STEREO</button></div>
+            <div>Light Chrome</div>
+            <div>106 dB SPL. 16Hz – 25kHz. 2 x 500 Watts RMS.</div>
+            <div><button class="panier">Ajouter au Panier</button></div>
+        </section>
     </div>
 </div>
 
@@ -32,6 +45,7 @@
     <div class="rigth"><p>Phantom I 103 dB, avec sa Remote intègre l’ensemble des technologies de pointe Devialet pour un son haute fidélité et une densité sonore hors norme dans un format aussi compact. Ressentez l’impact physique du son comme jamais auparavant.
             Avec un second Phantom I 103 dB en stéréo, la scène sonore s'étend encore, l’écoute devient encore plus puissante, plus extrême. Absolument immersif.</p></div>
 </div>
+
 
 </body>
 </html>
