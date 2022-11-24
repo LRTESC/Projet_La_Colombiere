a6f89b8e18124b2baab7896d6b90b52905e97bf2
B.Herbelles <brian.herbelles@le-campus-numerique.fr>
2022-11-24 12:07:30 +0100

    Product-list + ProductController change for database connection #1

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function show()
     {
-            return view("product-list",['css' => 'product-list']);
+//            return view("product-list",['css' => 'product-list']);
+        $product1=DB::select('select * from Product where idProduct = ?',[1]);
+        $product2=DB::select('select * from Product where idProduct = ?',[3]);
+        $product3=DB::select('select * from Product where idProduct = ?',[7]);
+
+        return view('product-list', [
+            'css' => 'product-list',
+            'product1' => $product1,
+            'product2' => $product2,
+            'product3' => $product3
+        ]);
     }
     public function id($id)
     {
