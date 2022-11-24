d923000b491a4766eae14674c3c55d28e36f46ec
B.Herbelles <brian.herbelles@le-campus-numerique.fr>
2022-11-24 13:43:50 +0100

    product-list alex

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,15 +10,10 @@ class ProductController extends Controller
     public function show()
     {
 //            return view("product-list",['css' => 'product-list']);
-        $product1=DB::select('select * from Product where idProduct = ?',[1]);
-        $product2=DB::select('select * from Product where idProduct = ?',[3]);
-        $product3=DB::select('select * from Product where idProduct = ?',[7]);
-
+        $products = DB::select('select * from Product where idProduct <= ?',[8]);
         return view('product-list', [
             'css' => 'product-list',
-            'product1' => $product1,
-            'product2' => $product2,
-            'product3' => $product3
+            'products' => $products
         ]);
     }
     public function id($id)
--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -76,190 +76,66 @@
     <section>
         <div class="column-product">
 
-            <div class="product-c-1">
-                <div class="img-space-around">
-                    <img class="img-space-product" src="{{asset('storage/pictures/Product1.webp')}}" alt="Product1">
+            @foreach ($products as $product)
+                <div class="product-c">
+                    <div class="img-space-around">
+                        <img class="img-space-product" src="{{asset('storage/pictures/Product' . $product->idProduct . '.webp')}}" alt="Product">
+                    </div>
+                    <div class="product-d-1">
+                            <h4 class="title-product-btmp">
+                                {{$product->Name}}
+                            </h4>
+                            <h5 class="title-product-perfspec">
+                                Performances
+                            </h5>
+                            <h6 class="spec-product">
+                                Puissance totale d’amplification
+                            </h6>
+                            <p class="info-product">
+                                500 W RMS
+                            </p>
+                            <h6 class="spec-product">
+                                Réponse en fréquence (bande-passante)
+                            </h6>
+                            <p class="info-product">
+                                Bande passante: 16Hz à 25kHz (@-6dB)<br>
+                                Précision de la réponse en fréquence: ± 2dB de 20Hz<br>
+                                à 20kHz
+                            </p>
+                            <h6 class="spec-product">
+                                Niveau sonore maximal
+                            </h6>
+                            <p class="info-product">
+                                103 dB SPL à 1 mètre
+                            </p>
+                            <h5 class="title-product-perfspec">
+                                Spécifications
+                            </h5>
+                            <h6 class="spec-product">
+                                Haut-parleurs
+                            </h6>
+                            <p class="info-product">
+                                Haut-Parleur Tweeter à dôme aluminium<br>
+                                Haut-Parleur Medium à dôme aluminium<br>
+                                Haut-Parleurs Grave à dôme aluminium
+                            </p>
+                            <h6 class="spec-product">
+                                Finition
+                            </h6>
+                            <p class="info-product">
+                                Corps blanc RAL 9016<br>
+                                Flasques latérales en inox poli<br>
+                                Haut-parleurs de grave blanc RAL 9016
+                            </p>
+                            <h4 class="price-product-list">
+                                {{$product->Price}}
+                            </h4>
+                            <div class="div-button">
+                                <a href="{{route('productid',['id' => $product->idProduct])}}"><button class="buy-button-product">ACHETER</button></a>
+                            </div>
+                    </div>
                 </div>
-                <div class="product-d-1">
-                    @foreach($product1 as $product1)
-                        <h4 class="title-product-btmp">
-                            {{$product1->Name}}
-                        </h4>
-                        <h5 class="title-product-perfspec">
-                            Performances
-                        </h5>
-                        <h6 class="spec-product">
-                            Puissance totale d’amplification
-                        </h6>
-                        <p class="info-product">
-                            500 W RMS
-                        </p>
-                        <h6 class="spec-product">
-                            Réponse en fréquence (bande-passante)
-                        </h6>
-                        <p class="info-product">
-                            Bande passante: 16Hz à 25kHz (@-6dB)<br>
-                            Précision de la réponse en fréquence: ± 2dB de 20Hz<br>
-                            à 20kHz
-                        </p>
-                        <h6 class="spec-product">
-                            Niveau sonore maximal
-                        </h6>
-                        <p class="info-product">
-                            103 dB SPL à 1 mètre
-                        </p>
-                        <h5 class="title-product-perfspec">
-                            Spécifications
-                        </h5>
-                        <h6 class="spec-product">
-                            Haut-parleurs
-                        </h6>
-                        <p class="info-product">
-                            Haut-Parleur Tweeter à dôme aluminium<br>
-                            Haut-Parleur Medium à dôme aluminium<br>
-                            Haut-Parleurs Grave à dôme aluminium
-                        </p>
-                        <h6 class="spec-product">
-                            Finition
-                        </h6>
-                        <p class="info-product">
-                            Corps blanc RAL 9016<br>
-                            Flasques latérales en inox poli<br>
-                            Haut-parleurs de grave blanc RAL 9016
-                        </p>
-                        <h4 class="price-product-list">
-                            {{$product1->Price}}
-                        </h4>
-                        <div class="div-button">
-                            <a href="{{route('productid',['id' => 'phantom-i-103db'])}}"><button class="buy-button-product">ACHETER</button></a>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-
-            <div class="product-c-2">
-                <div class="img-space-around">
-                    <img src="{{asset('storage/pictures/Product2.webp')}}" alt="Product2">
-                </div>
-                <div class="product-d-1">
-                    @foreach($product2 as $product2)
-                        <h4 class="title-product-btmp">
-                            {{$product2->Name}}
-                        </h4>
-                        <h5 class="title-product-perfspec">
-                            Performances
-                        </h5>
-                        <h6 class="spec-product">
-                            Puissance totale d’amplification
-                        </h6>
-                        <p class="info-product">
-                            1100 W RMS
-                        </p>
-                        <h6 class="spec-product">
-                            Réponse en fréquence (bande-passante)
-                        </h6>
-                        <p class="info-product">
-                            Bande passante: 14Hz à 27kHz (@-6dB)<br>
-                            Précision de la réponse en fréquence: ± 2dB de 20Hz<br>
-                            à 20kHz
-                        </p>
-                        <h6 class="spec-product">
-                            Niveau sonore maximal
-                        </h6>
-                        <p class="info-product">
-                            108 dB SPL à 1 mètre
-                        </p>
-                        <h5 class="title-product-perfspec">
-                            Spécifications
-                        </h5>
-                        <h6 class="spec-product">
-                            Haut-parleurs
-                        </h6>
-                        <p class="info-product">
-                            Haut-Parleur Tweeter à dôme titane pur de grade 1<br>
-                            Haut-Parleur Medium à dôme aluminium<br>
-                            Haut-Parleurs Grave à dôme aluminium
-                        </p>
-                        <h6 class="spec-product">
-                            Finition
-                        </h6>
-                        <p class="info-product">
-                            Corps blanc RAL 9016<br>
-                            Flasques latérales or rose plaquées 22 carat<br>
-                            Haut-parleurs de grave blanc RAL 9016
-                        </p>
-                        <h4 class="price-product-list">
-                            {{$product2->Price}}
-                        </h4>
-                        <div class="div-button">
-                            <a href="{{route('productid',['id' => 'phantom-i-108db'])}}"><button class="buy-button-product">ACHETER</button></a>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-
-            <div class="product-c-3">
-                <div class="img-space-around">
-                    <img src="{{asset('storage/pictures/Product3.webp')}}" alt="Product3">
-                </div>
-                <div class="product-d-1">
-                    @foreach($product3 as $product3)
-                        <h4 class="title-product-btmp">
-                            {{$product3->Name}}
-                        </h4>
-                        <h5 class="title-product-perfspec">
-                            Performances
-                        </h5>
-                        <h6 class="spec-product">
-                            Puissance totale d’amplification
-                        </h6>
-                        <p class="info-product">
-                            1100 Watts RMS
-                        </p>
-                        <h6 class="spec-product">
-                            Réponse en fréquence (bande-passante)
-                        </h6>
-                        <p class="info-product">
-                            Bande passante: 14Hz à 27kHz (@-6dB)<br>
-                            Précision de la réponse en fréquence: ± 2dB de 20Hz<br>
-                            à 20kHz
-                        </p>
-                        <h6 class="spec-product">
-                            Niveau sonore maximal
-                        </h6>
-                        <p class="info-product">
-                            108 dB SPL à 1 mètre
-                        </p>
-                        <h5 class="title-product-perfspec">
-                            Spécifications
-                        </h5>
-                        <h6 class="spec-product">
-                            Haut-parleurs
-                        </h6>
-                        <p class="info-product">
-                            Haut-Parleur Tweeter à dôme titane pur de grade 1<br>
-                            Haut-Parleur Medium à dôme aluminium<br>
-                            Haut-Parleurs Grave à dôme aluminium
-                        </p>
-                        <h6 class="spec-product">
-                            Finition
-                        </h6>
-                        <p class="info-product">
-                            Corps blanc RAL 9016<br>
-                            Flasques latérales recouvertes de feuilles de<br>
-                            moongold, alliage inoxydable de palladium et d’or
-                            jaune<br>
-                            Haut-parleurs de grave blanc RAL 9016
-                        </p>
-                        <h4 class="price-product-list2">
-                            {{$product3->Price}}
-                        </h4>
-                        <div class="div-button">
-                            <a href="{{route('productid',['id' => 'phantom-i-108db-opera'])}}"><button class="buy-button-product">ACHETER</button></a>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
+            @endforeach
 
         </div>
     </section>
