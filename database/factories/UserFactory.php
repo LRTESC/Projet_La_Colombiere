edd5eaaf97f8e6756d88b03123e8ef2e2f5bebe6
bricec-campus <brice.chaves@le-campus-numerique.fr>
2022-11-29 17:04:29 +0100

    iteration 8: cleaning project part 2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends Factory<User>
  */
-class UsersFactory extends Factory
+class UserFactory extends Factory
 {
     /**
      * Define the model's default state.
